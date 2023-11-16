ddc10219f01ae9f9214ce808cff595ab48cf552d
Haariiii <jh_lee111@naver.com>
2023-11-17 01:25:46 +0900

    231116

---
+++ Lv.1/가장 가까운 같은 글자.py
@@ -0,0 +1,7 @@
+def solution(s):
+    answer = [-1,]
+    for i in range(1,len(s)):
+        if s[:i].rfind(s[i]) != -1:
+            answer.append(i-s[:i].rfind(s[i]))
+        else: answer.append(-1)
+    return answer
